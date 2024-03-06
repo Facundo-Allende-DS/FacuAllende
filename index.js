663816b0bec0a8040862fcabd8c8ffb7506e53b5
FacuAllende <tympo.99@gmail.com>
2024-03-06 16:43:21 -0300

    Se agrega el porftolio a repositorio

---
+++ index.js
@@ -0,0 +1,8 @@
+function siguienteImagen() {
+    $('#carouselExampleIndicators').carousel('next');
+  }
+
+  // Función para cambiar a la imagen anterior del carrusel
+  function imagenAnterior() {
+    $('#carouselExampleIndicators').carousel('prev');
+  }
